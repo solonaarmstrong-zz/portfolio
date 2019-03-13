ac8dfd70315135959cef61c4664f4a371aa38124
Solona Armstrong <solona.armstrong@gmail.com>
2019-03-12 20:44:24 -0700

    Add card

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import FadeIn from '../FadeIn/FadeIn';
+import Card from '../Card/Card';
 
 interface Props {}
 interface State {}
@@ -8,7 +9,16 @@ class Skills extends Component<Props, State> {
   render() {
     return (
       <FadeIn duration={400}>
-        <div>Skills</div>
+        <br />
+        <br />
+        <br />
+        <Card title="Title">
+          Stuff inside the card
+          <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+          </ul>
+        </Card>
       </FadeIn>
     );
   }
