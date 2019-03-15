3b34823fc10895de8df6ec18b74e3782381643c2
Solona Armstrong <solona.armstrong@gmail.com>
2019-03-14 20:41:24 -0700

    Add about

---
+++ src/data/about-text.js
@@ -0,0 +1,14 @@
+const summaryText = {
+  content: [`I've been working on the web for a long time. To give you an idea how long, 
+  I built one of the first websites for LaSenza, the lingerie company, complete with a Flash 
+  introduction when it was cutting edge. I've evolved with the industry and now I specialize 
+  in front end development.`,
+  `If I had to choose only one word to describe myself, it would be resourceful. I often
+  find creative ways to solve problems. I also have a wide array of skills and interests,
+  like teaching and writing, that have all contributed to my value as a developer.`,
+  `I'm meticulous about details and appreciate high quality, both on screen and in code.
+  Having an impact motivates me, so I always give 100%.`,
+  ]
+}
+
+export default summaryText;
